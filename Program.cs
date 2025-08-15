258484c4ba89598445c42825f2ac0bb89cf3d379
ariwahidin <37979597+ariwahidin@users.noreply.github.com>
2025-08-16 04:52:41 +0700

    set culture

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using TMSBilling.Data;
 using TMSBilling.Models;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,12 @@ builder.Services.AddSession();
 builder.Services.AddScoped<SelectListService>();
 // Tambahkan sebelum var app = builder.Build();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+
+
+// 🔧 SET DEFAULT CULTURE
+var cultureInfo = new CultureInfo("id-ID");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 var app = builder.Build();
 
